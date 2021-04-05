c22369d56a6db706aa5227680ce41ce57cc168b2
Shanden Rosete-Agapay <>
2021-04-05 00:44:15 +0000

    Everything

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.get("/", function(req, res){
     });
 });
 
+
 app.get("/about", function(req, res){
   res.render("about", {aboutContent: aboutContent});
 });
@@ -68,11 +69,52 @@ app.get("/posts/:postName", function(req, res){
 
 });
 
-// need to write out an app.get function that open a route /weather Need an EJS view called weather.ejs that displays one text field to input city name
-//This EJS view will input a city name from user
-// Then need to write out an app.get function that will use the city name to query the Weather API to retrieve basic weather information - temperature, description and humidity
-// The display of the weather information must be saved to an array and then the results of the array must be pushed to the /weather EJS view to display
-// The /weather route and page created by weather.ejs page should allow for the input of the city name, and the display of the weather for the city - city name, weather icon image, temperature in F, description, humidity, wind direction
+let weathers = [];
+
+app.get("/weather", function(req,res){
+  res.render("weather", {
+    weathers:weathers
+  });
+});
+
+app.post("/weather", function(req,res){
+  weathers = []; 
+        const city = req.bod.city; 
+        const units = "imperial";
+        const apiKey = "c034340cdc5c48d783e206ed556e6058"
+        const url = "https://api.openweathermap.org/data/2.5/weather?APPID=" + apiKey + "&q=" +city+ "&units=" + units; 
+        console.log(city);
+
+        https.get(url, function(response){
+
+
+          response.on("data", function(data){
+            const weatherData = JSON.parse(data); 
+            const temp = weatherData.main.temp; 
+            const city = weatherData.name; 
+            const windDirection = weatherData.wind.deg; 
+            const weatherDescription = weatherData.weather[0].icon; 
+            const imageURL = 
+            "http://openweathermap.org/img/wn/" + icon + "@2x.png"; 
+
+
+            const weather = {
+              city: city, 
+              image: imageURL,
+              temp: temp,
+              weatherDescription: weatherDescription, 
+              humidity: humidity, 
+              windDirection: windDirection 
+            };
+
+            weathers.push(weather);
+            res.redirect("/weather");
+
+          });
+
+        });
+
+
 
 
 app.listen(3000, function() {
